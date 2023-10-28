d2b4466075562e1a730c5bcdd3b1b55ffbe4a2e0
Bosworth99 <josh@joshbosworth.com>
2023-10-28 14:54:03 -0700

    chore: add wanted files back

---
+++ README.md
@@ -0,0 +1,2 @@
+# s_a_f_e_t_y
+workbench for some art / game doodles with react, threejs and p5
---
+++ src/app.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 0;
+    background-color: #000000;
+    color: #ddd;
+`;
